9f6c33bf29940f91ae864a227573be28ecf99f13
yo <j_navarrete@yahoo.com>
2026-04-01 20:15:01 +0200

    Seleccionamos todos los elementos que necesitamos del html

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let loginSection = document.getElementById('loginSection');
+    let fechaSection = document.getElementById('fechaSection');
+    let recursosSection = document.getElementById('recursosSection');
+    let idUsuarioInput = document.getElementById('idUsuario');
+    let verificarBtn = document.getElementById('verificarBtn');
+    let loginMessage = document.getElementById('loginMessage');
+    let diaInput = document.getElementById('dia');
+    let mesInput = document.getElementById('mes');
+    let anoInput = document.getElementById('ano');
+    let enviarFechaBtn = document.getElementById('enviarFechaBtn');
+    let recursosSelect = document.getElementById('recursosSelect');
+    let reservarBtn = document.getElementById('reservarBtn');
+    let reservaMessage = document.getElementById('reservaMessage');
+
+    let idUsuario = null;
+    let fechaSeleccionada = null;
+})
